41c7533fecd7c210b9a618d2fe838c292de702fa
Bridjet Mendyuk <bjmendy@Bridjets-MBP.attlocal.net>
2017-01-29 20:46:40 -0600

    JavaScript

--- main2.js
+++ main2.js
@@ -2,9 +2,22 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+var cards = 4
 
-if (cardTwo !== cardFour) {
-	alert("Sorry, try again");
-} else if (cardTwo === cardFour) {
-	alert("You found a match!");
-};
+// if (cardTwo !== cardFour) {
+// 	alert("Sorry, try again");
+// } else if (cardTwo === cardFour) {
+// 	alert("You found a match!");
+// };
+
+var createCards = function() {
+	var gameBoard = document.getElementById('game-board');
+	var divs = 
+	for (var i = 0; i < cards; i++) {
+		document.createElement('div');
+		divs.className = 'card';
+		gameBoard.appendChild(divs);
+	}
+}
+
+createCards();
